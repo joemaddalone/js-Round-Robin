6e31aba2c2a42cc69d70175902698ad6393e67ec
Joe Maddalone <joe@insert-title.com>
2013-03-04 19:42:56 -0600

    425 bytes small

--- RoundRobin.js
+++ RoundRobin.js
@@ -1,50 +1,34 @@
 ﻿/*!
- * JavaScript Round Robin Generator
+ * JavaScript Round Robin Generator 1.0
  * http://insert-title.com/
  *
- * Copyright 2010, Joe Maddalone
+ * Copyright 2013, Joe Maddalone
  * Dual licensed under the MIT or GPL Version 2 licenses.
  *
  */
-
-function RoundRobin(teams) {
-    var i;
-    var ret = "";
-    var round;
-    var numplayers = 0;
-    numplayers = parseInt(teams) + parseInt(teams % 2);
-    numplayers = parseInt(numplayers);
-    var a = new Array(numplayers - 1);
-    var alength = a.length;
-    for (var x = 0; x < (numplayers) ; x++) { a[x] = "Team " + (x + 1); }
-    if (numplayers != parseInt(teams)) { a[alength] = "BYE"; }
-    var pos;
-    var pos2;
-    ret = "----- ROUND #1-----<br />"
-    for (var r1a = 0; r1a < (numplayers / 2) ; r1a++) {
-        ret += a[r1a] + " vs. " + a[alength - r1a] + "<br />"
-    }
-    for (round = 2; round < alength + 1; round++) {
-        ret += "<br /><br />----- ROUND #" + round + "-----<br />"
-        ret += a[0] + " vs. " + a[alength - (round - 1)] + "<br />"
-
-        for (i = 2; i < (numplayers / 2) + 1; i++) {
-            pos = (i + (round - 2))
-            if (pos >= alength) { pos = ((alength - pos)) * -1 }
-            else
-            { pos = (i + (round - 2)) }
-
-            pos2 = (pos - (round - 2)) - round
-            if (pos2 > 0) {
-                pos2 = (alength - pos2) * -1
-            }
-
-            if (pos2 < (alength * -1)) {
-                pos2 += alength
-            }
-            ret += a[(alength + pos2)]
-            ret += " vs. " + a[(alength - pos)] + "<br />"
+function RoundRobin(t) {
+    var e = []
+    , p=+t+(t%2)
+    , a = new Array(p - 1)
+    , l = a.length
+    , pos
+    , i
+    , r
+    , pos2;
+    for(x=p;x--;){a[x]=(x+1)}
+    p^t&&(a[p-1] = "_");
+    for (i = 0; i < (p / 2) ; i++) {
+        e.push({r:1,a:a[i],b:a[l - i]})
+    };
+    for (r = 2; r < l + 1; r++) {
+        e.push({r:r,a:a[0],b:a[l - (r - 1)]});
+        for (i = 2; i < (p / 2) + 1; i++) {
+            pos = (i + (r - 2)) >= l ? ((l - (i + (r - 2)))) * -1 : (i + (r - 2));
+            pos2 = (pos - (r - 2)) - r;
+            pos2 > 0&&(pos2 = (l - pos2) * -1);
+            pos2 < (l * -1)&&(pos2 += l);
+            e.push({r:r,a:a[(l + pos2)],b: a[(l - pos)]})
         }
     }
-    return ret
+    return e
 }
